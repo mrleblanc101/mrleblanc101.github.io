e31dcc3c5e737c1812e095f9971cf05529f22edb
Sébastien LeBlanc <sebastien.leblanc@libeo.com>
2023-01-07 22:42:01 -0500

    Add tags

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -3,39 +3,55 @@
         class="group relative rounded overflow-hidden bg-zinc-100 dark:bg-zinc-800"
         @mouseenter="is_expanded = true"
         @mouseleave="is_expanded = false"
+        @touchend="onTap"
     >
-        <component
-            :is="project.url ? 'a' : 'div'"
-            :href="project.url ? project.url : null"
-            :target="project.url ? '_blank' : null"
-        >
-            <!-- <div class="absolute p-4 text-2xl top-0 left-0 bg-red-500 text-white">{{ project.order }}</div> -->
-            <img
-                class="w-full aspect-[16/10] object-cover"
-                :src="`${project._path}/thumbnail.jpg`"
-                width="600"
-                height="365"
-                :alt="`Image: ${project.title}`"
-                loading="lazy"
-            />
+        <!-- <div class="absolute p-4 text-2xl top-0 left-0 bg-red-500 text-white">{{ project.order }}</div> -->
+        <img
+            class="w-full aspect-[16/10] object-cover"
+            :src="`${project._path}/thumbnail.jpg`"
+            width="600"
+            height="365"
+            :alt="`Image: ${project.title}`"
+            loading="lazy"
+        />
+        <div class="absolute top-0 bottom-0 left-0 w-full z-50 overflow-y-auto flex flex-col">
             <div
-                class="after:content-[''] after:absolute after:-top-[100%] after:bottom-0 after:-left-3 after:-right-3 md:after:-left-6 md:after:-right-6 after:bg-gradient-to-t after:from-black/70 after:to-transparent after:pointer-events-none absolute p-3 md:p-6 bottom-0 left-0 w-full after:-z-10 z-50"
+                class="relative mt-auto p-4 md:p-6 after:content-[''] after:absolute after:-top-[100%] after:bottom-0 after:left-0 after:right-0 after:bg-gradient-to-t after:from-black/70 after:to-transparent after:-z-10 after:pointer-events-none"
             >
-                <img class="relative max-h-[60px]" :src="`${project._path}/logo.svg`" :alt="`Logo: ${project.title}`" />
-                <Collapse :when="is_expanded" class="v-collapsed">
-                    <div class="mt-4">
-                        Donec sed odio dui. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Lorem ipsum
-                        dolor sit amet, consectetur adipiscing elit. Maecenas sed diam eget risus varius blandit sit
-                        amet non magna. Donec id elit non mi porta gravida at eget metus. Donec id elit non mi porta
-                        gravida at eget metus.
-                    </div>
-                </Collapse>
+                <img class="max-h-[60px]" :src="`${project._path}/logo.svg`" :alt="`Logo: ${project.title}`" />
             </div>
-        </component>
+            <Collapse :when="is_expanded" class="v-collapse">
+                <div class="p-4 md:p-6 pt-0 md:pt-0 bg-black/70 flex flex-col gap-3">
+                    <!-- <div class="text-gray-300">
+                            Donec sed odio dui. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Lorem
+                            ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed diam eget risus varius
+                            blandit sit amet non magna. Donec id elit non mi porta gravida at eget metus. Donec id elit
+                            non mi porta gravida at eget metus.
+                        </div> -->
+                    <ul class="flex flex-wrap gap-2">
+                        <li
+                            v-for="tag in project.tags"
+                            class="inline-block px-4 py-2 bg-gray-200 text-gray-700 font-medium text-xs uppercase rounded-full shadow-md whitespace-nowrap"
+                        >
+                            {{ tag }}
+                        </li>
+                    </ul>
+                    <a
+                        v-if="project.url"
+                        :href="project.url"
+                        target="_blank"
+                        class="inline-flex gap-1 hover:gap-2 font-bold transition-all before:content-[''] before:block [@media(any-hover:hover)]:before:absolute [@media(any-hover:hover)]:before:inset-0"
+                    >
+                        Voir le projet <IArrowRight class="w-5" />
+                    </a>
+                </div>
+            </Collapse>
+        </div>
     </li>
 </template>
 
 <script lang="ts" setup>
+import IArrowRight from '@/assets/svg/arrow-right.svg?component';
 const is_expanded = ref(false);
 
 defineProps({
@@ -45,10 +61,14 @@ defineProps({
         required: true,
     },
 });
+
+function onTap(event: TouchEvent) {
+    is_expanded.value = !is_expanded.value;
+}
 </script>
 
 <style lang="postcss" scoped>
-.v-collapsed {
-    transition: height var(--vc-auto-duration) cubic-bezier(0.3, 0, 0.6, 1);
+.v-collapse {
+    transition: height var(--vc-auto-duration) ease-out;
 }
 </style>
