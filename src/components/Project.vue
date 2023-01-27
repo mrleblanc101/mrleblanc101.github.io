9070355d583519684b11db2193611f6caaa4e377
Sébastien LeBlanc <sebastien.leblanc@libeo.com>
2023-01-26 21:07:02 -0500

    Use ClientOnly for Collapse component

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -24,29 +24,31 @@
             >
                 <img class="max-h-[60px]" :src="`${project._path}/logo.svg`" :alt="`Logo: ${project.title}`" />
             </div>
-            <Collapse :when="is_expanded" class="v-collapse">
-                <div class="p-4 md:px-5 md:py-4 pt-0 md:pt-0 bg-black/80 flex flex-col gap-3">
-                    <div v-if="project.description" class="text-gray-300">
-                        {{ project.description }}
-                    </div>
-                    <ul class="flex flex-wrap gap-2" v-if="project.tags && project.tags.length">
-                        <li
-                            v-for="tag in project.tags"
-                            class="inline-block px-4 py-2 bg-gray-200 text-gray-700 font-medium text-xs uppercase rounded-full shadow-md whitespace-nowrap"
+            <ClientOnly>
+                <Collapse :when="is_expanded" class="v-collapse">
+                    <div class="p-4 md:px-5 md:py-4 pt-0 md:pt-0 bg-black/80 flex flex-col gap-3">
+                        <div v-if="project.description" class="text-gray-300">
+                            {{ project.description }}
+                        </div>
+                        <ul class="flex flex-wrap gap-2" v-if="project.tags && project.tags.length">
+                            <li
+                                v-for="tag in project.tags"
+                                class="inline-block px-4 py-2 bg-gray-200 text-gray-700 font-medium text-xs uppercase rounded-full shadow-md whitespace-nowrap"
+                            >
+                                {{ tag }}
+                            </li>
+                        </ul>
+                        <a
+                            v-if="project.url"
+                            :href="project.url"
+                            target="_blank"
+                            class="inline-flex gap-1 font-bold before:content-[''] before:block [@media(any-hover:hover)]:before:absolute [@media(any-hover:hover)]:before:inset-0 text-gray-300"
                         >
-                            {{ tag }}
-                        </li>
-                    </ul>
-                    <a
-                        v-if="project.url"
-                        :href="project.url"
-                        target="_blank"
-                        class="inline-flex gap-1 font-bold before:content-[''] before:block [@media(any-hover:hover)]:before:absolute [@media(any-hover:hover)]:before:inset-0 text-gray-300"
-                    >
-                        Voir le projet <IArrowRight class="w-5" />
-                    </a>
-                </div>
-            </Collapse>
+                            Voir le projet <IArrowRight class="w-5" />
+                        </a>
+                    </div>
+                </Collapse>
+            </ClientOnly>
         </div>
     </li>
 </template>
